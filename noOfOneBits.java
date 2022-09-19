d52ad7e806a234adc48e102f1f530bdccdbb306d
Rao-Apoorva <apoorvarao2120@gmail.com>
2022-09-19 20:48:58 +0530

    feat: add no of one bits

---
+++ noOfOneBits.java
@@ -0,0 +1,12 @@
+public class Solution {
+    // you need to treat n as an unsigned value
+    
+    int i;
+    public int hammingWeight(int n) {
+        
+        
+        i=Integer.bitCount(n);
+                return i;
+
+    }
+}
